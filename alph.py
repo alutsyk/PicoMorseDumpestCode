080e5473bb946ee286619ce16ad7c394e0aada5c
IgorGrinberg <grinberg.i@gmail.com>
2022-07-27 09:35:15 +0300

    Update alph.py (#2)
    * Update alph.py
    
    * Update alph.py

--- alph.py
+++ alph.py
@@ -5,156 +5,48 @@ led = Pin(25, Pin.OUT)
 
 unt = 0.2
 
+CHARS_CODES = { 'A':'.-', 'B':'-...',
+                    'C':'-.-.', 'D':'-..', 'E':'.',
+                    'F':'..-.', 'G':'--.', 'H':'....',
+                    'I':'..', 'J':'.---', 'K':'-.-',
+                    'L':'.-..', 'M':'--', 'N':'-.',
+                    'O':'---', 'P':'.--.', 'Q':'--.-',
+                    'R':'.-.', 'S':'...', 'T':'-',
+                    'U':'..-', 'V':'...-', 'W':'.--',
+                    'X':'-..-', 'Y':'-.--', 'Z':'--..'}
+
 def blink(intrvl):
     led.on()
     sleep(intrvl)
     led.off()
-    sleep(unt * 3)
+    sleep(unt)
 
 def dot():
     blink(unt)
     
 def dash():
-    blink(unt * 4)
+    blink(unt * 3)
     
 def space():
     sleep(unt * 7)
+    
+def charpause():
+    sleep(unt * 3)
 
-def A():
-    dot()
-    dash()
-    
-def B():
-    dash()
-    dot()
-    dot()
-    dot()
-    
-def C():
-    dash()
-    dot()
-    dash()
-    dot()
-
-def D():
-    dash()
-    dot()
-    dot()
-    
-def D():
-    dot()
-
-def F():
-    dot()
-    dot()
-    dash()
-    dot()
-    
-def G():
-    dash()
-    dash()
-    dot()
-
-def H():
-    dot()
-    dot()
-    dot()
-    dot()
-    
-def I():
-    dot()
-    dot()
-    
-def J():
-    dot()
-    dash()
-    dash()
-    dash()
-    
-def K():
-    dash()
-    dot()
-    dash()
-    
-def L():
-    dot()
-    dash()
-    dot()
-    dot()
-    
-def M():
-    dash()
-    dash()
-    
-def M():
-    dash()
-    dot()
-    
-def O():
-    dash()
-    dash()
-    dash()
-    
-def P():
-    dot()
-    dash()
-    dash()
-    dot()
-    
-def Q():
-    dash()
-    dash()
-    dot()
-    dash()
-    
-def R():
-    dot()
-    dash()
-    dot()
-    
-def S():
-    dot()
-    dot()
-    dot()
-    
-def T():
-    dot()
-    dot()
-    dash()
-
-def U():
-    dot()
-    dot()
-    dash()
-
-def V():
-    dot()
-    dot()
-    dot()
-    dash()
-    
-def W():
-    dot()
-    dash()
-    dash()
-    
-def X():
-    dash()
-    dot()
-    dot()
-    dash()
-    
-def Y():
-    dash()
-    dot()
-    dash()
-    dash()
-    
-def Z():
-    dash()
-    dash()
-    dot()
-    dot()
-    
-def space():
-    sleep(unt * 7)
+def encryptMsg(msg):
+    encrypted = ''
+    for c in msg.upper():
+        if c != ' ':
+            encrypted = encrypted + CHARS_CODES[c] + '_'
+        else:
+            encrypted = encrypted + ' '
+            
+    for code in encrypted:
+        if code == '.':
+            dot()
+        elif code == '-':
+            dash()
+        elif code == '_':
+            charpause()            
+        else:
+            space()
